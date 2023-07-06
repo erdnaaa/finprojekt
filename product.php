0d449fc3bbd5a35a3c4dce4f410c58376dbe02b2
wickedr0se <andrejulian30@gmail.com>
2023-07-06 12:27:05 +0700

    product bootstrap5

--- product.php
+++ product.php
@@ -65,12 +65,6 @@
                   Reports
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
-                  Integrations
-                </a>
-              </li>
             </ul>
           </div>
         </nav>
@@ -123,6 +117,7 @@
             ?>
         </tbody>
         </table>
+		<a class="btn btn-primary" href="add_page.php" role="button">Add Product</a>
           </div>
         </main>
 </div>
