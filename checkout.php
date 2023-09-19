60f47767a20145da1d04d33ad3383c96ccfc096f
Andre <32614215+erdnaaa@users.noreply.github.com>
2023-09-19 13:58:52 +0700

    without price

--- checkout.php
+++ checkout.php
@@ -2,7 +2,7 @@
     session_start();
     include 'connect.php';
     $user_id = $_COOKIE['user_id'];
-    $product_id = $_POST['product_name'];
+    $product_name = $_POST['product_name'];
     $total_price= $_POST['total_price'];
     $qty = $_POST['qty'];
     
@@ -11,8 +11,8 @@
         // echo "<pre>"; print_r($_SESSION['shopping_cart']); echo "</pre>";
         $product_id = $product['id'];
         $price = $product['price'];
-        $query = "INSERT INTO pesanan (user_id, product_name, quantity, is_done, price) VALUES ('$user_id', '$product_name', '$qty', '0', '$price')";
-        
+        // $query = "INSERT INTO pesanan (user_id, product_name, quantity, is_done, price) VALUES ('$user_id', '$product_name', '$qty', '0', '$price')";
+        $query = "INSERT INTO pesanan (user_id, product_name, quantity, is_done) VALUES ('$user_id', '$product_name', '$qty', '0')";
         //pesanan user_id, product_id, quantity, is_done, price
             
         $result = $mysqli -> query($query); 
